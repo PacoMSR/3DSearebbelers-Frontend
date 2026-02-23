e1b09dd231c524c5a595d7eaa30929411853ce0b
PacoMSR <pmateu@searebbel.com>
2026-02-23 11:56:41 +0100

    security: move API tokens to environment variables

--- src/components/api.js
+++ src/components/api.js
@@ -2,6 +2,5 @@
 export const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather";
-export const WEATHER_API_KEY = "d9f2af726c42791cde01fe0a2751f711";
-export const MAPBOX_TOKEN =
-  "pk.eyJ1IjoiY2Fyb2xpbmEtZGNmIiwiYSI6ImNsNmRxZjg3dTAwcHczaW9yOGtwbTIzZ2cifQ.b9Ojg2GBupuiC_DkIoKiaA";
+export const WEATHER_API_KEY = process.env.REACT_APP_OPENWEATHER_API_KEY || "";
+export const MAPBOX_TOKEN = process.env.REACT_APP_MAPBOX_TOKEN || "";
